93612ad692fc03dfa4d08db74fc78ef98fc156a5
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-06 01:43:22 -0700

    t

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -1,7 +1,7 @@
-const cypress = require("cypress")
-
 describe('My First Test', () => {
     it('Does not do much!', () => {
-        cypress.visit()
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('CA')
+        cy.get('.product:visible').should('have.length', 4)
     })
   })
