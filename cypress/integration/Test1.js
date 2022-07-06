2faf9a913eabf76cc424bb32de7d2fb1574324be
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-06 01:54:35 -0700

    Cypress.get/type/find

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -2,6 +2,11 @@ describe('My First Test', () => {
     it('Does not do much!', () => {
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
         cy.get('.search-keyword').type('CA')
+        // Selenium get hit url in browser, cypress get acts like find element of selenium
         cy.get('.product:visible').should('have.length', 4)
+        // Parent child chaining
+        cy.get('.products').find('.product').should('have.length', 4)
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
     })
   })
