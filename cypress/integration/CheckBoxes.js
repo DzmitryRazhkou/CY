64db7d5d5eccc46719344c5b534204a3d6cee1e5
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-06 16:25:41 -0700

    f

--- cypress/integration/CheckBoxes.js
+++ cypress/integration/CheckBoxes.js
@@ -1,5 +1,6 @@
 describe('Practise Checkboxes', () => {
     it('Lets Try!', () => {
+        
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
