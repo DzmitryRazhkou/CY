52689837ba2751950c2b8b4300c335aec11b1ea5
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-08 15:08:41 -0700

    full

--- cypress/integration/BDD/eCommerce.feature
+++ cypress/integration/BDD/eCommerce.feature
@@ -3,6 +3,6 @@ Feature: End to end E-Commerce validation
 Scenario: E-Commerce products delivery
 
 Given I open ECommerce Page
-When I add items to cartd
+When I add items to cart
 And Validate the total prices
 Then Select the country submit and verify
---
+++ cypress/integration/BDD/eCommerce/eCommerceStepDef.js
@@ -0,0 +1,67 @@
+import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
+import HomePage from "../../pageObjects/HomePage";
+import ProductPage from "../../pageObjects/ProductPage";
+
+// Create new objects of pages:
+const homePage = new HomePage()
+const productPage = new ProductPage()
+
+Given('I open ECommerce Page', function()
+{
+    cy.visit('https://rahulshettyacademy.com/angularpractice/')
+})
+
+When('I add items to cart', function()
+{
+    homePage.getShopTab().click()
+
+    this.data.productName.forEach(function(ele){
+        cy.get('h4.card-title').each(($el, index, $list) => {
+            if($el.text().includes(ele))
+            {
+                cy.get('button.btn.btn-info').eq(index).click()
+            }
+        })
+    })
+    productPage.getCheckOut().click()
+})
+
+And('Validate the total prices', function()
+{
+    var sum = 0
+
+        cy.get('tr td:nth-child(4) strong').each(($el, index, $list) => {
+            const amount = $el.text()
+            var res = amount.split(" ")
+            res = res[1].trim()
+            sum = Number(sum) + Number(res)
+        }).then(function()
+        {
+            cy.log(sum)
+        })
+        cy.get('h3 strong').then(function(element)
+        {
+            const amount = element.text()
+            var res = amount.split(" ")
+            var total = res[1].trim()
+
+            expect(Number(total)).to.be.equal(sum)
+        })
+})
+
+Then('Select the country submit and verify', function()
+{
+    cy.get(':nth-child(4) > :nth-child(5) > .btn').click()
+    cy.get('#country').type(this.data.country)
+    cy.get('.suggestions > ul > li > a').click()
+    cy.get('input#checkbox2').check({force: true})
+    cy.get('.ng-untouched > .btn').click()
+    // cy.get('.alert').should('have.text', 'Success! Thank you! Your order will be delivered in next few weeks :-).')
+
+    // If you need to validate partial text!!!
+
+    cy.get('.alert').then(function(ele){
+        const actText = ele.text()
+        expect(actText.includes('Success')).to.be.true
+    })
+})
