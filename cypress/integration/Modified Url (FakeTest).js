62b20e6aadd87f4b1b35796bb9b120f4ddb700af
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-10 18:01:35 -0700

    modified url

---
+++ cypress/integration/Modified Url (FakeTest).js
@@ -0,0 +1,18 @@
+describe('Fake Test', () => {
+    it('XHR', () => {
+        cy.visit('https://rahulshettyacademy.com/angularAppdemo/')
+            cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty', (req) => 
+            {
+                req.url = 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=gans'
+
+                req.continue((res) => 
+                {
+                    // expect(res.statusCode).to.be(403)
+                })
+            }).as('dummyUrl')
+    
+
+            cy.get("button[class='btn btn-primary']").click()
+            cy.wait('@dummyUrl')
+        })  
+})
