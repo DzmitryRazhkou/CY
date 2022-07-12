39adead5f37d3545cb294f3880a13b5817820099
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-07-11 17:08:07 -0700

    all done

--- cypress/integration/JWT Session.js
+++ cypress/integration/JWT Session.js
@@ -10,5 +10,17 @@ describe('JWT Session', () => {
                 }
             })
         })
+
+        cy.get('div.card-body button:last-of-type').eq(2).click()
+        cy.get("[routerlink='/dashboard/cart']").click()
+        cy.contains('Checkout').click()
+        cy.get('.form-group > .input').type('Uk')
+        // cy.get('.ta-results list-group ng-star-inserted').each(($el, index, $list) => {
+        //     if($el.text === 'Ukraine'){
+
+        //         cy.wrap($el).click()
+        //     }
+        // })
+        cy.get('.btnn').click()
     })
 })
